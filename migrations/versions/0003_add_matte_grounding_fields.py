32a639dfd20920aca296819e6dfa16d08d4a83f3
Ross Goldberg <rpgoldberg@gmail.com>
2026-07-10 02:31:34 -0500

    Fix matted column server_default storing SQL string not boolean

--- migrations/versions/0003_add_matte_grounding_fields.py
+++ migrations/versions/0003_add_matte_grounding_fields.py
@@ -18,15 +18,11 @@ depends_on = None
 def upgrade() -> None:
     with op.batch_alter_table("image_versions") as batch_op:
         batch_op.add_column(
-            sa.Column("matted", sa.Boolean, nullable=False, server_default="false")
+            sa.Column("matted", sa.Boolean, nullable=False, server_default=sa.false())
         )
         batch_op.add_column(sa.Column("bottom_margin_frac", sa.Float, nullable=True))
-        batch_op.add_column(
-            sa.Column("contact_band_center_x_frac", sa.Float, nullable=True)
-        )
-        batch_op.add_column(
-            sa.Column("contact_band_width_frac", sa.Float, nullable=True)
-        )
+        batch_op.add_column(sa.Column("contact_band_center_x_frac", sa.Float, nullable=True))
+        batch_op.add_column(sa.Column("contact_band_width_frac", sa.Float, nullable=True))
         batch_op.add_column(sa.Column("thumbhash", sa.String(64), nullable=True))
         batch_op.add_column(sa.Column("dominant_color", sa.String(32), nullable=True))
 
